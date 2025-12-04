d84c8a9020e1fef7d25ae2e3840ceadad2af97ec
Vibhu Garg <vibhugarg@Vibhus-MacBook-Pro.local>
2025-12-03 21:43:52 -0800

    feat(cdk): Add DynamoDB and Cognito resources

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,16 +1,50 @@
-import * as cdk from 'aws-cdk-lib/core';
+import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as cognito from 'aws-cdk-lib/aws-cognito';
 
-export class CdkStack extends cdk.Stack {
+export class VantageStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // DynamoDB Table for Deals
+    const dealsTable = new dynamodb.Table(this, 'DealsTable', {
+      partitionKey: { name: 'userId', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: 'itemCategory', type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // NOT recommended for production
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Cognito User Pool
+    const userPool = new cognito.UserPool(this, 'VantageUserPool', {
+      selfSignUpEnabled: true,
+      signInAliases: { email: true },
+      autoVerify: { email: true },
+      passwordPolicy: {
+        minLength: 8,
+        requireLowercase: true,
+        requireUppercase: true,
+        requireDigits: true,
+      },
+    });
+
+    // Cognito User Pool Client
+    const userPoolClient = new cognito.UserPoolClient(this, 'VantageUserPoolClient', {
+      userPool,
+      generateSecret: false, // Mobile apps typically don't use client secrets
+    });
+
+    // Outputs
+    new cdk.CfnOutput(this, 'DealsTableName', {
+      value: dealsTable.tableName,
+    });
+
+    new cdk.CfnOutput(this, 'UserPoolId', {
+      value: userPool.userPoolId,
+    });
+
+    new cdk.CfnOutput(this, 'UserPoolClientId', {
+      value: userPoolClient.userPoolClientId,
+    });
   }
 }
